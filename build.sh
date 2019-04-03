dfd1d267d831e439d782a048670ee03d596cd0c7
Florent Biville <fbiville@pivotal.io>
2019-04-03 18:35:51 +0200

    [#33] Migrate to Go modules

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-dep ensure
+go install
 
 rm helper_mocks/*.go || true \
     && rm vcs_mocks/*.go || true \
     && rm fs_mocks/*.go || true \
     && rm core_mocks/*.go || true
 
-go get github.com/vektra/mockery/.../ \
-    && mockery -output helper_mocks -outpkg helper_mocks -dir helper -name Clock \
-    && mockery -output vcs_mocks -outpkg vcs_mocks -dir vcs -name Vcs \
-    && mockery -output vcs_mocks -outpkg vcs_mocks -dir vcs -name VersioningClient \
-    && mockery -output fs_mocks -outpkg fs_mocks -dir fs -name FileWriter \
-    && mockery -output fs_mocks -outpkg fs_mocks -dir fs -name FileReader \
-    && mockery -output fs_mocks -outpkg fs_mocks -dir fs -name File \
-    && mockery -output fs_mocks -outpkg fs_mocks -dir fs -name PathMatcher \
-    && mockery -output core_mocks -outpkg core_mocks -dir core -name ExecutionTracker
+GO111MODULE=off go get -u  github.com/vektra/mockery/.../ \
+    && GO111MODULE=on mockery -output helper_mocks -outpkg helper_mocks -dir helper -name Clock \
+    && GO111MODULE=on mockery -output vcs_mocks -outpkg vcs_mocks -dir vcs -name Vcs \
+    && GO111MODULE=on mockery -output vcs_mocks -outpkg vcs_mocks -dir vcs -name VersioningClient \
+    && GO111MODULE=on mockery -output fs_mocks -outpkg fs_mocks -dir fs -name FileWriter \
+    && GO111MODULE=on mockery -output fs_mocks -outpkg fs_mocks -dir fs -name FileReader \
+    && GO111MODULE=on mockery -output fs_mocks -outpkg fs_mocks -dir fs -name File \
+    && GO111MODULE=on mockery -output fs_mocks -outpkg fs_mocks -dir fs -name PathMatcher \
+    && GO111MODULE=on mockery -output core_mocks -outpkg core_mocks -dir core -name ExecutionTracker
 
 go build ./...
 go clean -testcache && go test -v ./...
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/fbiville/headache
+
+go 1.12
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-zglob v0.0.1
+	github.com/onsi/ginkgo v1.7.0
+	github.com/onsi/gomega v1.4.2
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.2.2
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v0.0.0-20180816142147-da425ebb7609
+	golang.org/x/net v0.0.0-20180921000356-2f5d2388922f // indirect
+	golang.org/x/sys v0.0.0-20190109145017-48ac38b7c8cb // indirect
+)
