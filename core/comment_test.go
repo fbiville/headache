0ea8ae00d05a1318f19035761ddb2335286c419b
Jimmy Leger <jleger@lafourchette.com>
2019-05-28 21:44:33 +0200

    Refactored comment.go tests

--- core/comment_test.go
+++ core/comment_test.go
@@ -3,35 +3,25 @@ package core_test
 import (
 	. "github.com/fbiville/headache/core"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Comment", func() {
 
-	It("matches Hash comment style", func() {
-		style := ParseCommentStyle("Hash")
+	DescribeTable("matches comment style",
+		func(name, openingStr, closingStr, str string) {
+			style := ParseCommentStyle(name)
 
-		Expect(style.GetName()).To(Equal("Hash"))
-		Expect(style.GetClosingString()).To(Equal(""))
-		Expect(style.GetOpeningString()).To(Equal(""))
-		Expect(style.GetString()).To(Equal("# "))
-	})
-
-	It("matches DashDash comment style", func() {
-		style := ParseCommentStyle("DashDash")
-
-		Expect(style.GetName()).To(Equal("DashDash"))
-		Expect(style.GetClosingString()).To(Equal(""))
-		Expect(style.GetOpeningString()).To(Equal(""))
-		Expect(style.GetString()).To(Equal("-- "))
-	})
-
-	It("matches SemiColon comment style", func() {
-		style := ParseCommentStyle("SemiColon")
-
-		Expect(style.GetName()).To(Equal("SemiColon"))
-		Expect(style.GetClosingString()).To(Equal(""))
-		Expect(style.GetOpeningString()).To(Equal(""))
-		Expect(style.GetString()).To(Equal("; "))
-	})
+			Expect(style.GetName()).To(Equal(name))
+			Expect(style.GetClosingString()).To(Equal(closingStr))
+			Expect(style.GetOpeningString()).To(Equal(openingStr))
+			Expect(style.GetString()).To(Equal(str))
+		},
+		Entry("matches SlashStar comment style", "SlashStar", "/*", " */", " * "),
+		Entry("matches SlashSlash comment style", "SlashSlash", "", "", "// "),
+		Entry("matches Hash comment style", "Hash", "", "", "# "),
+		Entry("matches DashDash comment style", "DashDash", "", "", "-- "),
+		Entry("matches SemiColon comment style", "SemiColon", "", "", "; "),
+	)
 })
